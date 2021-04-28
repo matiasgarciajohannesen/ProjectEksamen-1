4876b3f0a57d9e6d3ade46f600384a1b1a80b820
matiasgarciajohannesen <69842901+matiasgarciajohannesen@users.noreply.github.com>
2021-04-28 13:29:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const posts = document.querySelector(".blog");
  fetch(F1)
      .then(response => response.json())
      .then(data => formula(data))
-     .catch(error => posts.innerHTML = "ups that went wrong")
+     .catch(error => console.error(error))
 
 const formula = (blogs) => {
      console.log(blogs);
@@ -13,14 +13,19 @@ const formula = (blogs) => {
      let postDiv = ``
      for (blog of blogs) {
          console.log(blog)
+         let images = blog._embedded["wp:featuredmedia"]
+         console.log(images)
+         for (image of blog._embedded["wp:featuredmedia"]){
          postDiv =  `
          <div>
-         <h2>${blog.title.rendered}</h2>
-         <p>${blog.content.rendered}</p>
+         <a href="details.html?id=${blog.id}"><h2>${blog.title.rendered}</h2></a>
+         <a href="details.html?id=${blog.id}"><img src="${image.source_url}" alt="yeet"></a>
         </div>
         `;
+        posts.innerHTML += postDiv;
+        }
 
-         posts.innerHTML += postDiv;
+         
      }
  }
 
