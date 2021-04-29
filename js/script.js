6278fb6733ef0500d1a4a86808250889c84a31a3
matiasgarciajohannesen <69842901+matiasgarciajohannesen@users.noreply.github.com>
2021-04-29 14:12:41 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+const F1 = "https://matiasjohannesen.no/wp-json/wp/v2/posts?_embed=true";
+const posts = document.querySelector(".blog");
+
+ fetch(F1)
+     .then(response => response.json())
+     .then(data => formula(data))
+     .catch(error => console.error(error))
+
+const formula = (blogs) => {
+     console.log(blogs);
+     posts.innerHTML = "";
+     let postDiv = ``
+     for (blog of blogs) {
+         console.log(blog)
+         let images = blog._embedded["wp:featuredmedia"]
+         console.log(images)
+         for (image of blog._embedded["wp:featuredmedia"]){
+         postDiv =  `
+         <div>
+         <a href="details.html?id=${blog.id}"><h2>${blog.title.rendered}</h2></a>
+         <a href="details.html?id=${blog.id}"><img src="${image.source_url}" alt="yeet"></a>
+        </div>
+        `;
+        posts.innerHTML += postDiv;
+        }
+
+         
+     }
+ }
