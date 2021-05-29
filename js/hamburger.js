c3516d1f4d0c9df02162e314f13d405f40bd1f6d
matiasgarciajohannesen <69842901+matiasgarciajohannesen@users.noreply.github.com>
2021-05-29 17:19:54 +0200

    style

--- js/hamburger.js
+++ js/hamburger.js
@@ -13,7 +13,7 @@ const navSlide = () => {
             if (link.style.animation) {
                 link.style.animation = ""
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+                link.style.animation = `navLinkFade 0.3s ease forwards ${index / 7 + 0.5}s`;
             }
         });
     });
